c587b413dd0ea8d673e8f6bcd6ac8e22342a2e95
Soares <svivi502@gmail.com>
2022-05-13 14:28:23 -0300

    criando Class Livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -2,13 +2,57 @@ package br.com.dio;
 
 import br.com.dio.model.Gato;
 
+import javax.swing.text.html.ListView;
+
 public class PrimeiroPrograma {
     public static void main(String[] args) {
         Gato gato = new Gato();
         System.out.println(gato);
+
+        Livro livro1 = new Livro("Biblia", 300);
+
+        System.out.println( livro1);
 
      /*       int a = 5;
             int b = 3;
             System.out.println("Hello Galera!" + (a+b));*/
     }
 }
+
+class Livro {
+
+    private String nome;
+    private Integer numPaginas;
+
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
+    }
+}
+
+
+
